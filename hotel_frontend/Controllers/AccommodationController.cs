6f614c16c6d550b461e4a6c6db3f320f1380d2a6
Thiện Ngô Trung <21522624@ms.uit.edu.vn>
2024-04-24 08:42:28 +0700

    acc

---
+++ hotel_frontend/Controllers/AccommodationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel_frontend.Controllers
+{
+    public class AccommodationController : Controller
+    {
+        public IActionResult Accommodation()
+        {
+            return View();
+        }
+    }
+}
