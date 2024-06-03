f8ed333bbde3fecec8534686af5493366787f535
Thiện Ngô Trung <21522624@ms.uit.edu.vn>
2024-06-03 11:54:20 +0700

    All booking room process interfaces are completed.

--- hotel_frontend/obj/Debug/net8.0/hotel_frontend.AssemblyInfo.cs
+++ hotel_frontend/obj/Debug/net8.0/hotel_frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel_frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9288485135cac6ca0003c6cca42647509ade27a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef5fa6bd93745697138c961f14c09ef01a769ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel_frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel_frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
